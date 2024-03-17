9ba0c11ac6ad0b53e23492a6717f35e7d371f203
Sunny Kumar <sunny.kumar@enterprisedb.com>
2024-03-17 21:49:59 +0000

    Python_1 solution for PreparatoryContent-module3
    Add solution for PreparatoryContent-module03-Python_1
    
    Signed-off-by: Sunny Kumar <sunny.kumar@enterprisedb.com>

---
+++ PreparatoryContent/module03/001.py
@@ -0,0 +1,14 @@
+###Python_1
+Description
+Remove the leading spaces from the string  input_str = '         This is my first code'
+###
+
+# Reading the input as a string; ignore the following two lines
+import ast, sys
+input_str = sys.stdin.read()
+
+# Write your code here
+final_str = input_str.lstrip()#Type your answer here
+print(final_str)
+###
+
