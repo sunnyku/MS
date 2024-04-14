b6752ea5eeefc98917854586b8d941fa9973beed
Sunny Kumar <sunny.kumar@enterprisedb.com>
2024-04-14 19:48:56 +0100

    Add solution for numpy_session
    Signed-off-by: Sunny Kumar <sunny.kumar@enterprisedb.com>

---
+++ PreparatoryContent/numpy/005.py
@@ -0,0 +1,11 @@
+Array 'arange' Function
+Description
+Create an array of first 10 multiples of 5 using the 'arange' function.
+Execution Time Limit
+15 seconds
+
+
+import numpy as np
+array_multipleof5 = np.arange(5,51,5)#Type your answer here
+
+print(array_multipleof5)
